f40fafcd6c5e6fdd65ee2bb236ecc86fc09340e4
DelcioCoder <armandodelcio40@gmail.com>
2025-02-28 21:08:56 +0100

    fix(auth): fix async cookie retrieval in NavbarWrapper

--- src/components/NavbarWrapper/index.tsx
+++ src/components/NavbarWrapper/index.tsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import Navbar from "../Navbar";
 
 export default async function NavbarWrapper() {
-  const accessToken: boolean = cookies().get("access_token")?.value;
+  const accessToken = (await cookies()).get("access_token")?.value;
   const isAuthenticated = !!accessToken;
 
   return <Navbar isAuthenticated={isAuthenticated} />;
