573c3a0ab158c1dd6cffdc6bb9b8eb03c809b73d
DelcioCoder <armandodelcio40@gmail.com>
2025-03-02 16:34:27 +0100

    feat(schemas): add opening_hours to HotelSchema and define UserSchema
    - Added  opening_hours field to HotelSchema
    - Created Userschema with id and username fields
    - Updated types for better type inference

--- schemas.ts
+++ schemas.ts
@@ -26,6 +26,7 @@ export const HotelSchema = z.object({
     id: z.number(),
     name: z.string(),
     description: z.string().nullable(),
+    opening_hours: z.string().nullable(),
     rating: z.number().optional(),
     images: z.array(ImageSchema).optional(), // Um hotel pode ter várias imagens
     date_added: z.string(),
@@ -33,5 +34,14 @@ export const HotelSchema = z.object({
     centrality: z.number(),
 })
 
+
+export const UserSchema = z.object({
+    id: z.number(),
+    username: z.string()
+})
+
 export type ImageType = z.infer<typeof ImageSchema>;
 export type RestaurantType = z.infer<typeof RestaurantSchema>;
+export type HotelType = z.infer<typeof HotelSchema>;
+export type UserType = z.infer<typeof UserSchema>;
+
